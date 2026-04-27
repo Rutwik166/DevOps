edb4f4b74edddacb862a846b0e3cbebfe0f40c3e
Rutwik Bhoge <rutwikbhoge079@gmail.com>
2026-04-27 17:11:54 +0530

    Refactor Main.java formatting
    Removed unnecessary line breaks and adjusted formatting.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub! From Rutwik");
+        System.out.println("Hello, GitHub! From Rutwik")
         System.out.println("I am learning Jenkins!!");
     }
 }
